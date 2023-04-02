41bd50a518fb7a9248699c3c3c1efd24cd1564ab
_Duck-Noris_ <58949193+Ryn-Bghl@users.noreply.github.com>
2023-04-02 13:39:30 +0200

    wordle.js created 🐱‍💻✔

---
+++ wordle.js
@@ -0,0 +1,45 @@
+var dictionary = {
+  q: [1, 1],
+  w: [1, 2],
+  e: [1, 3],
+  r: [1, 4],
+  t: [1, 5],
+  y: [1, 6],
+  u: [1, 7],
+  i: [1, 8],
+  o: [1, 9],
+  p: [1, 10],
+  a: [2, 1],
+  s: [2, 2],
+  d: [2, 3],
+  f: [2, 4],
+  g: [2, 5],
+  h: [2, 6],
+  j: [2, 7],
+  k: [2, 8],
+  l: [2, 9],
+  enter: [3, 1],
+  z: [3, 2],
+  x: [3, 3],
+  c: [3, 4],
+  v: [3, 5],
+  b: [3, 6],
+  n: [3, 7],
+  m: [3, 8],
+  delete: [3, 9],
+};
+
+const words = ["ratio", "mends", "lucky"];
+
+function write(char) {
+  document.querySelector(
+    `div.Game-keyboard-row:nth-child(${dictionary[char][0]}) > div:nth-child(${dictionary[char][1]})`
+  ).click();
+}
+
+words.forEach((word) => {
+  for (let i = 0; i < word.length; i++) {
+    write(`${word[i]}`);
+  }
+  write("enter");
+});
