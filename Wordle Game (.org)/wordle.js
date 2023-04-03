907f09557ecab5eaf25b465170904052a9d2fff7
_Duck-Noris_ <58949193+Ryn-Bghl@users.noreply.github.com>
2023-04-03 15:31:23 +0200

    🐱‍💻

--- Wordle Game (.org)/wordle.js
+++ Wordle Game (.org)/wordle.js
@@ -1,36 +1,7 @@
 // only use in https://wordlegame.org/
 
 // all the letter and their coordinates
-var dictionary = {
-  q: [1, 1],
-  w: [1, 2],
-  e: [1, 3],
-  r: [1, 4],
-  t: [1, 5],
-  y: [1, 6],
-  u: [1, 7],
-  i: [1, 8],
-  o: [1, 9],
-  p: [1, 10],
-  a: [2, 1],
-  s: [2, 2],
-  d: [2, 3],
-  f: [2, 4],
-  g: [2, 5],
-  h: [2, 6],
-  j: [2, 7],
-  k: [2, 8],
-  l: [2, 9],
-  enter: [3, 1],
-  z: [3, 2],
-  x: [3, 3],
-  c: [3, 4],
-  v: [3, 5],
-  b: [3, 6],
-  n: [3, 7],
-  m: [3, 8],
-  delete: [3, 9],
-};
+var dictionary = {q: [1, 1], w: [1, 2], e: [1, 3], r: [1, 4], t: [1, 5], y: [1, 6], u: [1, 7], i: [1, 8], o: [1, 9], p: [1, 10], a: [2, 1], s: [2, 2], d: [2, 3], f: [2, 4], g: [2, 5], h: [2, 6], j: [2, 7], k: [2, 8], l: [2, 9], enter: [3, 1], z: [3, 2], x: [3, 3], c: [3, 4], v: [3, 5], b: [3, 6], n: [3, 7], m: [3, 8], delete: [3, 9]};
 
 // the starting words
 const words = ["ratio", "mends", "lucky"];
@@ -49,3 +20,13 @@ words.forEach((word) => {
   }
   write("enter");
 });
+
+// Create a new <p> element
+var paragraph = document.createElement("p");
+
+// Set the content of the <p> element
+var text = document.createTextNode("This is a paragraph.");
+paragraph.appendChild(text);
+
+// Add the <p> element to the document
+document.body.appendChild(paragraph);
